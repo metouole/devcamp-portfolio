a5357e23f2bedc9517b4fa3f169efc1067956abe
SOMDA Métouolé Mwinbé <yvesomda93@yahoo.fr>
2019-11-14 14:41:12 +0000

    Implementing edit and update portfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -21,4 +21,22 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def edit
+  	@portfolio_item = Portfolio.find(params[:id])
+  end
+
+   def update
+   	@portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image))
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+        format.json { render :show, status: :ok, location: @portfolio_item }
+      else
+        format.html { render :edit }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 end
