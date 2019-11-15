38b72887877152a6a2899667bb19902b88be27cc
SOMDA Métouolé Mwinbé <yvesomda93@yahoo.fr>
2019-11-15 02:35:12 +0000

    Adding delete action to portfolio features

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -30,17 +30,28 @@ class PortfoliosController < ApplicationController
   	end
 
    def update
-   	@portfolio_item = Portfolio.find(params[:id])
-    respond_to do |format|
+	   	@portfolio_item = Portfolio.find(params[:id])
+	    respond_to do |format|
 
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image))
-        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
-        format.json { render :show, status: :ok, location: @portfolio_item }
-      else
-        format.html { render :edit }
-        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+	      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image))
+	        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+	        format.json { render :show, status: :ok, location: @portfolio_item }
+	      else
+	        format.html { render :edit }
+	        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+	      end
+	    end
+   end
+
+     def destroy
+     	@portfolio_item = Portfolio.find(params[:id])
+	    @portfolio_item.destroy
+	    respond_to do |format|
+	      format.html { redirect_to portfolios_url, notice: 'Portfolio Item was successfully destroyed.' }
+	    end
+	  end
+
+
+
 
 end
