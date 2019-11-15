36c1c1ff5a03e314664cfc0ac0b19dc970d7e64e
SOMDA Métouolé Mwinbé <yvesomda93@yahoo.fr>
2019-11-15 02:07:12 +0000

    Implementing show functionnality for portfolio module

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,21 +9,25 @@ class PortfoliosController < ApplicationController
 	end
 
 	def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image))
+	    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image))
 
-    respond_to do |format|
-      if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Portfolio Item was successfully created.' }
-      else
-        format.html { render :new }
-        
-      end
-    end
-  end
+	    respond_to do |format|
+	      if @portfolio_item.save
+	        format.html { redirect_to portfolios_path, notice: 'Portfolio Item was successfully created.' }
+	      else
+	        format.html { render :new }
+	        
+	      end
+	    end
+ 	end
 
-  def edit
-  	@portfolio_item = Portfolio.find(params[:id])
-  end
+    def edit
+  		@portfolio_item = Portfolio.find(params[:id])
+  	end
+
+  	def show
+  		@portfolio_item = Portfolio.find(params[:id])
+  	end
 
    def update
    	@portfolio_item = Portfolio.find(params[:id])
