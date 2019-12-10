658834f31819c4a272b30f81afe348354702d3a8
SOMDA Métouolé Mwinbé <yvesomda93@yahoo.fr>
2019-12-10 22:00:58 +0000

    Implementing data default

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,12 @@ class Portfolio < ApplicationRecord
 	end
 
 	scope :ruby_on_rails_portfolio, -> { where(subtitle: "Ruby On Rails") }
+
+	after_initialize :set_default
+
+	def set_default
+		self.main_image ||= "https://via.placeholder.com/600x400"
+		self.thumb_image ||= "https://via.placeholder.com/300x150"
+	end
 end
+
