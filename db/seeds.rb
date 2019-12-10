fc616bbc1a891762e27109b47f5de72c35460234
SOMDA Métouolé Mwinbé <yvesomda93@yahoo.fr>
2019-12-10 21:40:53 +0000

    Implemented custom scope to portfolio model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+3.times do |topic|
+	Topic.create!(
+		title: "topic #{topic}"
+		)
+end
+
+puts "3 topic created successfully"
+
 10.times do |blog|
 	Blog.create(
 		title: "My blog post #{blog}",
@@ -17,10 +25,20 @@ end
 
 puts "5 skills mentionned"
 
-9.times do |portfolio|
+8.times do |portfolio|
 	Portfolio.create(
 		title: "Portfolio title: #{portfolio}",
-		subtitle: "My great service",
+		subtitle: "Ruby On Rails",
+		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+		main_image: "https://via.placeholder.com/600x400",
+		thumb_image: "https://via.placeholder.com/300x150"
+		)
+end
+
+1.times do |portfolio|
+	Portfolio.create(
+		title: "Portfolio title: #{portfolio}",
+		subtitle: "Angular",
 		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 		main_image: "https://via.placeholder.com/600x400",
 		thumb_image: "https://via.placeholder.com/300x150"
