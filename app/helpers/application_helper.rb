11c4d4a22466e4f7a9f6d61798f7d5a2b7997aac
macbook <yvesomda93@yahoo.fr>
2020-02-17 19:27:19 +0000

    Implemented refactor for navigation and active status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,4 +21,46 @@ module ApplicationHelper
 	def copyright_generator
 		MetouoleViewTool::Renderer.copyright 'SOMDA Metouole Mwinbé', 'All rights reserved'
 	end
+
+	def nav_items
+		[
+			{
+				url: root_path,
+				title: 'Home'
+			},
+			{
+				url: about_path,
+				title: 'About'
+			},
+			{
+				url: blogs_path,
+				title: 'Blog'
+			},
+			{
+				url: portfolios_path,
+				title: 'Portfolio'
+			},
+			{
+				url: contact_path,
+				title: 'Contact'
+			}
+		]
+	end
+
+	def nav_helper style, tag_type
+		nav_links = ''
+
+		nav_items.each do |item|
+			nav_links << "<#{tag_type}><a href='#{item[:url]}'' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+		end
+		
+		
+		
+		nav_links.html_safe
+	end
+
+	def active? path
+		"active" if current_page? path
+	end
+
 end
