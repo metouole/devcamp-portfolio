93319cdc6f2d09ee5f7b2112f3a83ec97d373010
macbook <yvesomda93@yahoo.fr>
2020-02-18 20:55:11 +0000

    Implemented persistence positions updates for drag and drop interface

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,10 +11,55 @@ require("jquery")
 require("jquery-ui")
 import './bootstrap_custom.js'
 
-$(function(){
-  $('.sortable').sortable();
-});
 
+
+
+var ready, set_positions;
+
+set_positions = function(){
+    // loop through and give each task a data-pos
+    // attribute that holds its position in the DOM
+    $('.card').each(function(i){
+        $(this).attr("data-pos",i+1);
+    });
+}
+
+ready = function(){
+
+	//call set_positions function
+	set_positions();
+
+
+  	$('.sortable').sortable();
+
+  	// after the order changes
+    $('.sortable').sortable().bind('sortupdate', function(e, ui) {
+        // array to store new order
+        var updated_order = [];
+        // set the updated positions
+        set_positions();
+
+        // populate the updated_order array with the new task positions
+        $('.card').each(function(i){
+            updated_order.push({ id: $(this).data("id"), position: i+1 });
+        });
+
+        // send the updated order via ajax
+        $.ajax({
+            type: "PUT",
+            url: '/portfolios/sort',
+            data: { order: updated_order }
+        });
+    });
+
+};
+
+
+$(document).ready(ready);
+/**
+ * if using turbolinks
+ */
+$(document).on('page:load', ready);
 //= require jquery
 //= require jquery3
 //= require jquery-ui/widgets/sortable
